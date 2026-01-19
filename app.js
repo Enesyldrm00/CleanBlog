78f168698fbb5e6a2829f0ba6d3545fbd714bebc
Enesyldrm00 <enesbb137@gmail.com>
2026-01-19 15:48:50 +0300

    connect MongoDB and add Post CRUD with Mongoose

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const ejs = require('ejs');
-const app = express();
-app.use(express.static('public'));
+const mongoose = require('mongoose');
+const Post = require('./Gonder/post');
 
+const app = express();
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Template Engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index');
+//Router
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 app.get('/post', (req, res) => {
   res.render('post');
@@ -18,7 +30,11 @@ app.get('/add_post', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about');
 });
-
+app.post('/add_post', async (req, res) => {
+   console.log(req.body);   
+  await Post.create(req.body);
+  res.redirect('/');
+});
 const port = 3000;
 app.listen(port, () => {
   console.log('sunucu başladı');
